322c31faff687b6f3ec015ac994130b348489f76
ChouMonyrith <rithmony877@gmail.com>
2025-06-17 15:40:39 +0700

    Fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -80,7 +80,7 @@ function App() {
         acc + cur.points, 0
     )
     useEffect(() => {
-        fetch(`${import.meta.env.VITE_API_URL}/questions`)
+        fetch(`http://localhost:8000/questions`)
             .then(res => res.json())
             .then(data => {
                 dispatch({type: "dataReceived", payload: data});
